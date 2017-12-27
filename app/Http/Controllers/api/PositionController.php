abddf2bbbf793fa2a656e0289aa333ac6e059a83
Benedict Florance <benedictflorance.a@gmail.com>
2017-12-28 01:31:58 +0530

    Define method for adding or updating new positions and removed something rule

--- app/Http/Controllers/api/PositionController.php
+++ app/Http/Controllers/api/PositionController.php
@@ -4,8 +4,107 @@ namespace App\Http\Controllers\api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Validator;
+use App\User;
+use App\Rake;
+use App\Coach;
+use App\CoachStatus;
+use App\Position;
 
 class PositionController extends Controller
 {
     //
+	public function store(Request $request){
+		try{
+			$rules=[
+				'coach_num' => 'required|max:15',
+				'line' => 'max:127',
+				'stage' => 'numeric|digits_between:1,2'
+
+			];
+			$fields=collect(['line','stage']);
+			foreach ($fields as $field) {
+				$rules[$field].= '|required_without_all:' . implode(',', $fields->whereNotIn(null, [$field])->toArray());
+			}
+			$validator = Validator::make($request->all(),$rules);
+			if($validator->fails())
+			{
+				$errors = $validator->errors();
+				return response(["errors" => $errors,
+					"status" => 400]);
+			}
+			else
+			{
+				$user=User::where('token','=',$request->input('token'))->first();
+				if($user->role == 'write' || $user->role == 'admin')
+				{
+					$coach = Coach::where('coach_num',$request->input('coach_num'))->first();
+					if($coach)
+					{
+						$coach_id=$coach->id;
+						$status = Position::where('coach_id',$coach_id)->first();
+						if($status)
+						{
+							$data = $request->only(['line','stage']);
+							Position::where('coach_id',$coach_id)->first()->update($data);
+							$message="Position has been updated successfully for ".$coach->coach_num;
+						}
+						else
+						{
+							$data = array_merge($request->only(['line','stage']), ['coach_id' => $coach_id]);
+							Position::create($data);
+							$message="Position has been added successfully for ".$coach->coach_num;
+
+						}
+						$data=['message' => $message];
+						$status=200;
+						return response(["data" => $data, "status" => $status]);
+					}
+					else
+					{
+						$errors[]=[
+							'title' => 'Coach does not exist',
+						];
+						return  response([
+							'errors' => $errors,
+							'status' => 400,
+						]);	
+					}
+				} 
+				else
+				{
+					$errors[]=[
+						'title' => 'Unauthorized to add new positions',
+					];
+					return  response([
+						'errors' => $errors,
+						'status' => 401,
+					]);
+				}
+			}
+		}
+		catch(Exception $error){
+			$title = $error->getMessage();
+			$errors[]=['title' => $title];
+			return response(["errors" => $errors,
+				"status" => 500]);
+		}
+	}     
+	public function getall()
+	{
+		try
+		{ 
+			$positions=Position::all();
+			foreach($positions as $position)
+				$position->coach_num = Coach::where('id',$position->coach_id)->first()->coach_num;
+			return  response(['data' => $positions,'status' => 200,]);  
+		}
+
+		catch(Exception $error){
+			$title = $error->getMessage();
+			$errors[]=['title' => $title];
+			return response(["errors" => $errors,
+				"status" => 500]);
+		}   
+	}
 }
